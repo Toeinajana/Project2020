cff49e665ddcde6cd9e2f02835791dd698b4a586
Nantawan G <toeinajana@hotmail.com>
2020-09-17 14:30:22 +0700

    deleted inrelevant file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Home from './components/Home'
-import Order from './components/Order'
 import DetailPage from './components/DetailPage'
 function App() {
   return (
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,11 @@
 import React, { Component } from 'react'
-import AppBar from './AppBar'
-import "../components/Home.css"
-
 
 export class Home extends Component {
     render() {
         return (
-           
-           
-              
-                 <div>
-                 <AppBar/>
-                <h1>Welcome to Hunt4Health</h1>
-                 </div>
-               
+            <div>
+                
+            </div>
         )
     }
 }
